302b8437ba05bd338f401c2e8bc5fbb231477147
Val-Belous <vaaalbelous@gmail.com>
2022-06-28 22:22:05 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import playsound from 'play-sound';
 import searchMusic from './api/shazamApi';
 import template from './templates/musicCards.hbs';
 
 const form = document.querySelector('.form');
 const ul = document.querySelector('.container');
+const target = document.querySelector('.target-guard');
+let queryInput = '';
+let page = 0;
+
+const options = {
+  root: null,
+  rootMargin: '400px',
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(updateCards, options);
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const queryInput = event.target.elements.query.value;
-  searchMusic(queryInput).then(data =>
-    ul.insertAdjacentHTML('beforeend', template(data.tracks))
-  );
+  ul.innerHTML = '';
+  page = 0;
+  observer.unobserve(target);
+  queryInput = event.target.elements.query.value;
+  searchMusic(queryInput).then(data => {
+    ul.insertAdjacentHTML('beforeend', template(data.tracks));
+    observer.observe(target);
+  });
+}
+
+function updateCards(entries) {
+  // console.log(entries);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      searchMusic(queryInput, page).then(data => {
+        ul.insertAdjacentHTML('beforeend', template(data.tracks));
+      });
+    }
+  });
 }
